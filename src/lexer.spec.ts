14b7b1c3e1b67a56f9e506a0e0e30f5fc8b1c52c
Phil Fearon <pgfearo@gmail.com>
2019-12-26 10:09:39 +0000

    generate tests - initialise

--- src/lexer.spec.ts
+++ src/lexer.spec.ts
@@ -2,10 +2,11 @@ import { Lexer, Token, CharLevelState, TokenLevelState } from './Lexer'
 
 test('number token', () => {
     let l: Lexer = new Lexer();
-    let t: Token = { 
+
+    let t1: Token = { 
       value: "255", 
       charType: CharLevelState.lNl, 
       tokenType: TokenLevelState.Number
     };
-    expect(l.analyse('255')[0]).toEqual(t);
+    expect(l.analyse('255+union')[0]).toEqual(t1);
 })
